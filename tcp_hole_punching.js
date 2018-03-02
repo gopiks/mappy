3f81e45d570436ae8a1da87de3b515b1185d12ac
gopiks <gopi@g-square.in>
2018-03-02 13:35:29 +0530

    commit

--- tcp_hole_punching.js
+++ tcp_hole_punching.js
@@ -4,4 +4,4 @@ const app = express()
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
-/* To add code here.. the code shoult print external ip and port */
+/* To add code here.. the code shoult print external ip and port instead of 3000 (which is internal port) */
